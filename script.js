c5bb79712c88e0f833861583805aa011c104593a
taishiasami123 <taishi.asami123@gmail.com>
2020-02-13 18:37:48 +0900

    appidを修正

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = new URL("https://api.openweathermap.org/data/2.5/weather")
 const params = {
-    appid: "89d0efa5995aed5301b02f38f8736abb",
+    appid: "4b5774e9f3d2a07b84f0f2f88e486224",
     q: "London"
 };
 const qs = new URLSearchParams(params);
